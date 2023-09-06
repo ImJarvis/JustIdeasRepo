b1abd505f73358c1505a8b15921aaef5953dca15
Starkoo7 <its.me.siddh@gmail.com>
2023-09-06 11:44:04 +0530

    Create Print_natural_numbers_multithread_advanced.cpp
    Advanced generic solution for Printing Natural numbers in a multithreaded environment.

---
+++ Print_natural_numbers_multithread_advanced.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+const int MAX_NUMBER = 100;  // Print numbers up to 20
+
+std::mutex mtx;
+int currentNumber = 1;
+
+void printNumbers(int threadId, int totalThreads) {
+    while (true) {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if (currentNumber > MAX_NUMBER) {
+            break; // Stop when all numbers are printed
+        }
+
+        if ((currentNumber % totalThreads) == threadId) {
+            std::cout << "Thread " << threadId << ": " << currentNumber << std::endl;
+            currentNumber++;
+        }
+    }
+}
+
+int main() {
+    
+    const int totalThreads = std::thread::hardware_concurrency();;  // Number of threads supported 
+
+    std::thread threads[totalThreads];
+
+    for (int i = 0; i < totalThreads; ++i) {
+        threads[i] = std::thread(printNumbers, i, totalThreads);
+    }
+
+    for (int i = 0; i < totalThreads; ++i) {
+        threads[i].join();
+    }
+
+    return 0;
+}
